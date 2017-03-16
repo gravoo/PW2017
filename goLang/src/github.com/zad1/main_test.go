46eff6bc790804064cd1ac05dd7c2e4f79b86b5d
gravoo <gravoo@yahoo.com>
2017-03-16 23:20:54 +0100

    concept draft refinement

--- goLang/src/github.com/zad1/main_test.go
+++ goLang/src/github.com/zad1/main_test.go
@@ -8,6 +8,6 @@ import (
 func TestTrain(t *testing.T) {
         s := Train{1,2,3}
         fmt.Println(" created train :) ", s.velocity)
-        fmt.Println(" created train :) ", s.velocity)
+        fmt.Println(" created train :) ", s)
 }
 
