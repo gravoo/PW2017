2364bd28d91659ec2f2fdb409439b60066e437ac
gravoo <gravoo@yahoo.com>
2017-03-23 22:39:35 +0100

    further improvement on go implementation

--- goLang/src/github.com/zad1/main.go
+++ goLang/src/github.com/zad1/main.go
@@ -2,52 +2,70 @@ package main
 
 import "fmt"
 import "time"
+import "container/ring"
 
-func train(trainCh, steeringCh chan int, d time.Duration, val int){
+type Train struct{
+    maxVelocity int
+    maxCapacity int
+    currentPosition chan int
+    track *ring.Ring
+}
+
+func(t* Train)  travelTrough(d time.Duration){
 	for {
+        t.track = t.track.Next()
+        fmt.Println("Train is going to track", t.track.Value)
 		time.Sleep(d)
-		trainCh <- val
-		fmt.Println(<-steeringCh)
+		t.currentPosition <-t.track.Value.(int)
+		fmt.Println("Received value form steeringA", <-t.currentPosition)
 	}
 }
 type Steering struct{
 	timeToReconfig time.Duration
+    trains chan int
 }
 
-func (s* Steering) assignTrainToTrack(trainCh, steeringCh, trackCh chan int, steeringName string){
-	for x:= range trainCh {
+func (s* Steering) assignTrainToTrack(steeringName string){
+	for x:= range s.trains {
 		switch x{
 		case 1:
 			fmt.Println("train", x, "is going to route", x, "in steering", steeringName)
-				steeringCh<-10
-				trackCh<-10
+				s.trains<-1
 				time.Sleep(s.timeToReconfig)
 		case 2:
 			fmt.Println("train", x, "is going to route", x, "in steering", steeringName)
-				steeringCh<-20
-				trackCh<-20
+				s.trains<-2
 				time.Sleep(s.timeToReconfig)
 		}
 
 	}
 }
+type Track struct{
+    trackId int
+    in chan int
+}
 
-func track(trackCh chan int){
+func (tr* Track) track(){
 	for{
-		fmt.Println("Track is free, ok", <-trackCh)
+		fmt.Println("Track is free, ok", <-tr.in)
 		time.Sleep(5000*time.Millisecond)
 	}
 }
 
 func main() {
 	trainCh := make(chan int)
-	steeringCh := make(chan int)
 	trackCh := make(chan int)
-    steering := Steering{4*time.Second}
-	go train(trainCh, steeringCh, 2000*time.Millisecond, 1)
-	go train(trainCh, steeringCh, 4000*time.Millisecond, 2)
-	go steering.assignTrainToTrack(trainCh, steeringCh, trackCh, "steeringA")
-	go track(trackCh)
+    trainATrack := ring.New(2)
+    trainATrack.Value = 1
+    trainATrack = trainATrack.Next()
+    trainATrack.Value = 2
+    steeringA := Steering{4*time.Second, trainCh}
+    trainA:= Train{5, 5, trainCh, trainATrack}
+    trackA := Track{1, trackCh}
+
+	go trainA.travelTrough(2000*time.Millisecond)
+	go steeringA.assignTrainToTrack("steeringA")
+	go trackA.track()
 	time.Sleep(20000*time.Millisecond)
 
 	var input string
