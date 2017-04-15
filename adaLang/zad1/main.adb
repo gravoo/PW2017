555997c3bf5bfabdc900f3cf16c25a2c5076a408
gravoo <gravoo@yahoo.com>
2017-04-15 14:45:03 +0200

    added name for train

--- adaLang/zad1/main.adb
+++ adaLang/zad1/main.adb
@@ -7,6 +7,10 @@ procedure Main is
         entry Inicialize(An_Name : in SU.Unbounded_String);
     end Train;
 
+    task type Steering is 
+        entry Inicialize(An_Name : in SU.Unbounded_String);
+    end Steering;
+
     task body Train is
         TrainName : SU.Unbounded_String;
     begin
@@ -16,7 +20,19 @@ procedure Main is
             Ada.Text_IO.Put_Line(SU.To_String(TrainName));
         end Inicialize;
     end Train;
+
+    task body Steering is
+        SteeringName : SU.Unbounded_String;
+    begin
+        accept Inicialize(An_Name : SU.Unbounded_String) do 
+            SteeringName := An_Name;
+            Ada.Text_IO.Put("Inicialize ");
+            Ada.Text_IO.Put_Line(SU.To_String(SteeringName));
+        end Inicialize;
+    end Steering;
+    SteeringA: Steering;
     TrainA: Train;
 begin
     TrainA.Inicialize(SU.To_Unbounded_String("TrainA"));
+    SteeringA.Inicialize(SU.To_Unbounded_String("SteeringA"));
 end Main;
