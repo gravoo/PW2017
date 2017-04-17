ebc08bf8b4e5ec67ff08fd4489727b0bbe9a50be
gravoo <gravoo@yahoo.com>
2017-04-17 16:29:33 +0200

    added train id in assignTrain entry in track

--- adaLang/zad1/main.adb
+++ adaLang/zad1/main.adb
@@ -9,16 +9,17 @@ procedure Main is
     package SU renames Ada.Strings.Unbounded;
 
     task type Train is 
-        entry Inicialize(An_Name : in SU.Unbounded_String; An_SteeringIndexes : Integer_Vectors.Vector);
+        entry Inicialize(An_Name : in SU.Unbounded_String;
+        An_SteeringIndexes : Integer_Vectors.Vector; An_TrackId : Natural);
     end Train;
 
     task type Steering is 
         entry Inicialize(An_Name : in SU.Unbounded_String);
-        entry AssignTrainToTrack(An_TrackId : in Integer; An_TrainName : in SU.Unbounded_String);
+        entry AssignTrainToTrack(An_TrackId : in Integer; An_TrainId : in Natural);
     end Steering;
 
     task type StopTrack is 
-        entry AssignTrain;
+        entry AssignTrain(An_TrainId : in Natural);
     end StopTrack;
 
     Steerings: array (0..0) of  Steering;
@@ -29,16 +30,20 @@ procedure Main is
     task body Train is
         TrainName : SU.Unbounded_String;
         SteeringIndexes: Integer_Vectors.Vector;
+        TrainId: Natural;
     begin
-        accept Inicialize(An_Name : SU.Unbounded_String; An_SteeringIndexes : Integer_Vectors.Vector) do 
+        accept Inicialize(An_Name : SU.Unbounded_String; An_SteeringIndexes : Integer_Vectors.Vector ;
+                          An_TrackId : Natural) do 
             TrainName := An_Name;
+            TrainId := An_TrackId;
             SteeringIndexes := An_SteeringIndexes;
             Ada.Text_IO.Put("Inicialize ");
             Ada.Text_IO.Put_Line(SU.To_String(TrainName));
             Ada.Text_IO.Put("Target steeing:");
 			Ada.Integer_Text_IO.Put(SteeringIndexes.First_Element);
             Ada.Text_IO.Put_Line("");
-            Steerings(SteeringIndexes.First_Element).AssignTrainToTrack(SteeringIndexes.First_Element, TrainName);
+            Steerings(SteeringIndexes.First_Element).AssignTrainToTrack(SteeringIndexes.First_Element,
+            An_TrackId);
         end Inicialize;
     end Train;
 
@@ -50,20 +55,20 @@ procedure Main is
             Ada.Text_IO.Put("Inicialize ");
             Ada.Text_IO.Put_Line(SU.To_String(SteeringName));
         end Inicialize;
-        accept AssignTrainToTrack(An_TrackId : in Integer; An_TrainName : in SU.Unbounded_String) do 
+        accept AssignTrainToTrack(An_TrackId : in Integer; An_TrainId : in Natural) do 
             Ada.Text_IO.Put("Train request ");
-            Ada.Text_IO.Put(SU.To_String(An_TrainName));
+			Ada.Integer_Text_IO.Put(An_TrainId);
+            Ada.Text_IO.Put_Line(" ");
 			Ada.Integer_Text_IO.Put(An_TrackId);
             Ada.Text_IO.Put(" assign to track ");
-            Ada.Text_IO.Put_Line(SU.To_String(An_TrainName));
             Ada.Text_IO.Put_Line("");
-            StopTracks(An_TrackId).AssignTrain;
+            StopTracks(An_TrackId).AssignTrain(An_TrainId);
         end AssignTrainToTrack;
     end Steering;
 
     task body StopTrack is
     begin
-        accept AssignTrain do
+        accept AssignTrain(An_TrainId : in Natural) do
             Ada.Text_IO.Put_Line("Hello from stop track");
         end AssignTrain;
     end StopTrack;
@@ -71,5 +76,5 @@ begin
     SteeringIndexesForTrain0.Append(0);
     SteeringIndexesForTrain0.Append(1);
     Steerings(0).Inicialize(SU.To_Unbounded_String("SteeringA"));
-    Trains(0).Inicialize(SU.To_Unbounded_String("TrainA"), SteeringIndexesForTrain0);
+    Trains(0).Inicialize(SU.To_Unbounded_String("TrainA"), SteeringIndexesForTrain0, 0);
 end Main;
