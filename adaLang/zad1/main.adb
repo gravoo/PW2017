face25064a80a4fd9339f23ae452a27b8bd9d1f3
gravoo <gravoo@yahoo.com>
2017-04-17 15:09:16 +0200

    added stop track

--- adaLang/zad1/main.adb
+++ adaLang/zad1/main.adb
@@ -7,17 +7,23 @@ use Ada.Containers;
 procedure Main is
     package Integer_Vectors is new Vectors(Natural, Integer);
     package SU renames Ada.Strings.Unbounded;
+
     task type Train is 
         entry Inicialize(An_Name : in SU.Unbounded_String; An_SteeringIndexes : Integer_Vectors.Vector);
     end Train;
 
     task type Steering is 
         entry Inicialize(An_Name : in SU.Unbounded_String);
-        entry Hello;
+        entry AssignTrainToTrack(An_TrackId : in Integer; An_TrainName : in SU.Unbounded_String);
     end Steering;
+
+    task type StopTrack is 
+        entry AssignTrain;
+    end StopTrack;
 
     Steerings: array (0..0) of  Steering;
     Trains: array (0..0) of  Train;
+    StopTracks: array (0..0) of StopTrack;
     SteeringIndexesForTrain0 : Integer_Vectors.Vector;
 
     task body Train is
@@ -32,7 +38,7 @@ procedure Main is
             Ada.Text_IO.Put("Target steeing:");
 			Ada.Integer_Text_IO.Put(SteeringIndexes.First_Element);
             Ada.Text_IO.Put_Line("");
-            Steerings(SteeringIndexes.First_Element).Hello;
+            Steerings(SteeringIndexes.First_Element).AssignTrainToTrack(SteeringIndexes.First_Element, TrainName);
         end Inicialize;
     end Train;
 
@@ -44,10 +50,23 @@ procedure Main is
             Ada.Text_IO.Put("Inicialize ");
             Ada.Text_IO.Put_Line(SU.To_String(SteeringName));
         end Inicialize;
-        accept Hello do 
-            Ada.Text_IO.Put_Line("Hello");
-        end Hello;
+        accept AssignTrainToTrack(An_TrackId : in Integer; An_TrainName : in SU.Unbounded_String) do 
+            Ada.Text_IO.Put("Train request ");
+            Ada.Text_IO.Put(SU.To_String(An_TrainName));
+			Ada.Integer_Text_IO.Put(An_TrackId);
+            Ada.Text_IO.Put(" assign to track ");
+            Ada.Text_IO.Put_Line(SU.To_String(An_TrainName));
+            Ada.Text_IO.Put_Line("");
+            StopTracks(An_TrackId).AssignTrain;
+        end AssignTrainToTrack;
     end Steering;
+
+    task body StopTrack is
+    begin
+        accept AssignTrain do
+            Ada.Text_IO.Put_Line("Hello from stop track");
+        end AssignTrain;
+    end StopTrack;
 begin
     SteeringIndexesForTrain0.Append(0);
     SteeringIndexesForTrain0.Append(1);
