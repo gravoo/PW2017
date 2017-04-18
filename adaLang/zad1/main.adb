1e3ae79943be35963e8ae6fcf00cba089d1967ae
gravoo <gravoo@yahoo.com>
2017-04-18 19:19:10 +0200

    changed approach of solving puzzle

--- adaLang/zad1/main.adb
+++ adaLang/zad1/main.adb
@@ -3,101 +3,69 @@ with Ada.Strings.Unbounded;
 with Ada.Containers.Vectors;
 with Ada.Integer_Text_IO;
 use Ada.Containers;
+use Ada.Text_IO;
 
 procedure Main is
     package Integer_Vectors is new Vectors(Natural, Integer);
     package SU renames Ada.Strings.Unbounded;
+    type Train_ID is range 1..4;
+    type Track_ID is range 1..20;
+    type Steering_ID is range 1..10;
 
-    task type Train is 
-        entry Inicialize(An_Name : in SU.Unbounded_String; An_SteeringIndexes : Integer_Vectors.Vector;
-                         An_TrackId : Natural);
-        entry StartTrain;
-        entry WaitOnStopTrack;
-    end Train;
+    task type Train(ID : Train_ID; My_Steering: Steering_ID; Next_Steering: Steering_ID) is end Train;
+    type Train_Access is access Train;
 
-    task type Steering is 
-        entry Inicialize(An_Name : in SU.Unbounded_String);
-        entry AssignTrainToTrack(An_TrackId : in Integer; An_TrainId : in Natural);
-    end Steering;
-
-    task type StopTrack is 
-        entry AssignTrain(An_TrainId : in Natural);
+    protected type StopTrack is 
+        entry Wait_For_Clear;
+        entry Assign_Train(ID : Train_ID);
+    private
+        Clear: Boolean := True;
     end StopTrack;
 
-    Steerings: array (0..0) of  Steering;
-    Trains: array (0..0) of  Train;
-    StopTracks: array (0..0) of StopTrack;
-    SteeringIndexesForTrain0 : Integer_Vectors.Vector;
+    task type Steering is 
+        entry Request_TravelThroug(TrainID : in Train_ID; Next_Steering : in Steering_ID;
+        My_TrackId : out Track_ID);
+    end Steering;
+
+    Steerings: array (Steering_ID) of Steering;
+    StopTracks: array (Track_ID) of StopTrack;
 
     task body Train is
-        TrainName : SU.Unbounded_String;
-        SteeringIndexes: Integer_Vectors.Vector;
-        TrainId: Natural;
+        My_TrackId : Track_ID;
     begin
-        accept Inicialize(An_Name : SU.Unbounded_String;
-                          An_SteeringIndexes : Integer_Vectors.Vector;
-                          An_TrackId : Natural) do 
-            TrainName := An_Name;
-            TrainId := An_TrackId;
-            SteeringIndexes := An_SteeringIndexes;
-            Ada.Text_IO.Put("Inicialize ");
-            Ada.Text_IO.Put_Line(SU.To_String(TrainName));
-        end Inicialize;
-        loop 
-            select
-            accept StartTrain do
-                Ada.Text_IO.Put_Line("");
-                Ada.Text_IO.Put("Target steeing:");
-                Ada.Integer_Text_IO.Put(SteeringIndexes.First_Element);
-                Ada.Text_IO.Put_Line("");
-                Steerings(SteeringIndexes.First_Element).AssignTrainToTrack(
-                             SteeringIndexes.First_Element, TrainId);
-                Ada.Text_IO.Put_Line("Next step");
-            end StartTrain;
-            or
-            accept WaitOnStopTrack do
-                Ada.Text_IO.Put_Line("Waiting on stop track");
-            end WaitOnStopTrack;
-            end select;
-        end loop;
+        Steerings(My_Steering).Request_TravelThroug(ID, Next_Steering, My_TrackId);
+        Put_Line("Train: "& Train_ID'Image (ID) & " on track:" & Track_ID'Image(My_TrackId));
+        delay 10.0;
     end Train;
 
     task body Steering is
         SteeringName : SU.Unbounded_String;
     begin
-        accept Inicialize(An_Name : SU.Unbounded_String) do 
-            SteeringName := An_Name;
-            Ada.Text_IO.Put("Inicialize ");
-            Ada.Text_IO.Put_Line(SU.To_String(SteeringName));
-        end Inicialize;
-        loop 
-            accept AssignTrainToTrack(An_TrackId : in Integer; An_TrainId : in Natural) do 
-                Ada.Text_IO.Put("Train request ");
-                Ada.Integer_Text_IO.Put(An_TrackId);
-                Ada.Text_IO.Put_Line(" ");
-                Ada.Text_IO.Put("assign to track ");
-                Ada.Integer_Text_IO.Put(An_TrainId);
-                Ada.Text_IO.Put_Line("");
-                StopTracks(An_TrackId).AssignTrain(An_TrainId);
-                Ada.Text_IO.Put_Line("Assigned to track");
-            end AssignTrainToTrack;
+        loop
+            accept Request_TravelThroug(TrainID : in Train_ID; Next_Steering : in Steering_ID;
+                My_TrackId : out Track_ID)
+                do 
+                StopTracks(1).Assign_Train(TrainID);
+                My_TrackId := 1;
+            end Request_TravelThroug;
         end loop;
     end Steering;
 
-    task body StopTrack is
-    begin
-        loop
-            accept AssignTrain(An_TrainId : in Natural) do
-                Ada.Text_IO.Put("Hello from stop track train:");
-                Ada.Integer_Text_IO.Put(An_TrainId);
-                Ada.Text_IO.Put_Line("");
-            end AssignTrain;
-        end loop;
+    protected body StopTrack is
+        entry Wait_For_Clear
+        when Clear is
+        begin
+            null;
+        end;
+        entry Assign_Train(ID : Train_ID)
+        when Clear is
+        begin
+            Clear := False;
+        end;
     end StopTrack;
+
+    New_Train : Train_Access;
+
 begin
-    SteeringIndexesForTrain0.Append(0);
-    SteeringIndexesForTrain0.Append(1);
-    Steerings(0).Inicialize(SU.To_Unbounded_String("SteeringA"));
-    Trains(0).Inicialize(SU.To_Unbounded_String("TrainA"), SteeringIndexesForTrain0, 0);
-    Trains(0).StartTrain; 
+    New_Train := new Train(1,1,2);
 end Main;
