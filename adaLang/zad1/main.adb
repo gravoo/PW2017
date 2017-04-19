860316f2769694e33b92959655aece18737b6b23
gravoo <gravoo@yahoo.com>
2017-04-20 00:47:50 +0200

    completed basic case scenario

--- adaLang/zad1/main.adb
+++ adaLang/zad1/main.adb
@@ -12,12 +12,11 @@ procedure Main is
     type Steering_ID is range 1..20;
     type Track_Type is (Stop_Track, Drive_Track);
 
-
     protected type TrackThread(ID : Track_ID; My_TrackType : Track_Type) is 
         entry Wait_For_Clear;
-        entry Assign_Train(ID : Train_ID);
+        entry Assign_Train(TrainID : Train_ID);
         entry Check_TrackType(TrackType : out Track_Type);
-        entry Release_Track(ID : Train_ID);
+        entry Release_Track(TrainID : Train_ID);
     private
         Clear: Boolean := True;
     end TrackThread;
@@ -37,7 +36,7 @@ procedure Main is
     Steerings : Steering_Vector.Vector;
     Train1Route : Steering_Vector.Vector;
 
-    task type Train(ID : Train_ID;  Next_Steering: Steering_ID) is
+    task type Train(ID : Train_ID) is
         entry Init(Init_Route : in Steering_Vector.Vector);
     end Train;
     type Train_Access is access Train;
@@ -54,16 +53,23 @@ procedure Main is
         My_Track : Track_Access;
         My_TrackType : Track_Type;
         My_Steering: Steering_ID;
+        Next_Steering: Steering_ID;
     begin
         accept Init(Init_Route : in Steering_Vector.Vector) do
             My_Route := Init_Route;
         end Init;
-        My_Steering := My_Route.First_Index;
-        My_Route(My_Steering).Request_TravelThroug(ID, Next_Steering, My_Track);
-        My_Track.Check_TrackType(My_TrackType);
-        Put_Line("Train: "& Train_ID'Image (ID) & " on track");
-        delay 1.0;
-        My_Track.Release_Track(ID);
+        loop
+            for I in Steering_ID range My_Route.First_Index .. My_Route.Last_Index - 1 loop
+                Put_Line("Train");
+                My_Steering := My_Route(I).ID;
+                Next_Steering := My_Route(I+1).ID; 
+                My_Route(My_Steering).Request_TravelThroug(ID, Next_Steering, My_Track);
+                My_Track.Check_TrackType(My_TrackType);
+                Put_Line("Train: "& Train_ID'Image (ID) & " on track");
+                delay 5.0;
+                My_Track.Release_Track(ID);
+            end loop;
+        end loop;
     end Train;
 
     task body Steering is
@@ -75,6 +81,8 @@ procedure Main is
         loop
             accept Request_TravelThroug(TrainID : in Train_ID; Next_Steering : in Steering_ID;
                    Track_InUse : out Track_Access)  do 
+                    Put_Line("MY STEERING ID:" & Steering_ID'Image(ID));
+                    Put_Line("MY STEERING ID:" & Steering_ID'Image(Next_Steering));
                     My_Neighbours(Next_Steering).Assign_Train(TrainID);
                     Track_InUse := My_Neighbours(Next_Steering); 
             end Request_TravelThroug;
@@ -87,17 +95,19 @@ procedure Main is
         begin
             null;
         end;
-        entry Assign_Train(ID : Train_ID)
+        entry Assign_Train(TrainID : Train_ID)
         when Clear is
         begin
-            Put_Line("TrackThread task; train: "& Train_ID'Image (ID) & " on track: " & Track_Type'Image(My_TrackType));
+            Put_Line("TrackThread task; train: "& Train_ID'Image (TrainID) & " on track: " & Track_ID'Image(ID)
+                & " " & Track_Type'Image(My_TrackType));
             Clear := False;
         end;
-        entry Release_Track(ID : Train_ID)
+        entry Release_Track(TrainID : Train_ID)
         when not Clear is
         begin
             Clear := True;
-            Put_Line("TrackThread task; Train: "& Train_ID'Image (ID) & " released track");
+            Put_Line("TrackThread task; Train: "& Train_ID'Image (TrainID) &
+                " released track " & Track_Type'Image(My_TrackType) & " " & Track_ID'Image(ID));
         end;
         entry Check_TrackType(TrackType : out Track_Type)
         when not Clear is
@@ -117,13 +127,25 @@ begin
 
     Steering1Neighbours.Append(Tracks(1));
     Steering1Neighbours.Append(Tracks(2));
+    Steering1Neighbours.Append(null);
+    Steering1Neighbours.Append(null);
+    Steering1Neighbours.Append(null);
 
+    Steering2Neighbours.Append(Tracks(1));
     Steering2Neighbours.Append(Tracks(2));
     Steering2Neighbours.Append(Tracks(3));
+    Steering2Neighbours.Append(null);
+    Steering2Neighbours.Append(null);
 
+    Steering3Neighbours.Append(null);
+    Steering3Neighbours.Append(Tracks(2));
     Steering3Neighbours.Append(Tracks(3));
     Steering3Neighbours.Append(Tracks(4));
+    Steering3Neighbours.Append(null);
 
+    Steering4Neighbours.Append(null);
+    Steering4Neighbours.Append(null);
+    Steering4Neighbours.Append(Tracks(3));
     Steering4Neighbours.Append(Tracks(4));
     Steering4Neighbours.Append(Tracks(5));
 
@@ -141,10 +163,11 @@ begin
     Train1Route.Append(Steerings(2));
     Train1Route.Append(Steerings(3));
     Train1Route.Append(Steerings(4));
+    Train1Route.Append(Steerings(4));
     Train1Route.Append(Steerings(3));
     Train1Route.Append(Steerings(2));
     Train1Route.Append(Steerings(1));
-    Trains.Append(new Train(1,2));
+    Trains.Append(new Train(1));
 
     Trains(1).Init(Train1Route);
 end Main;
