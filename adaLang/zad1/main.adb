9e53ac2c70e32b7462a0da421313dc280aa212d5
gravoo <gravoo@yahoo.com>
2017-04-20 23:07:46 +0200

    first implementation for one train completed

--- adaLang/zad1/main.adb
+++ adaLang/zad1/main.adb
@@ -12,14 +12,18 @@ procedure Main is
     type Steering_ID is range 1..20;
     type Track_Type is (Stop_Track, Drive_Track);
 
-    protected type TrackThread(ID : Track_ID; My_TrackType : Track_Type) is 
+    protected type TrackThread(ID : Track_ID; My_TrackType : Track_Type; Wait_Time : Integer; Track_Max_Velocity : Integer; 
+        Track_Length : Integer) is 
         entry Wait_For_Clear;
         entry Assign_Train(TrainID : Train_ID);
         entry Check_TrackType(TrackType : out Track_Type);
         entry Release_Track(TrainID : Train_ID);
+        entry Wait_OnStation(Wait_OnTrackTime : out Integer);
+        entry Drive_Trough(Length : out Integer; MaxVelocity : out Integer);
     private
         Clear: Boolean := True;
     end TrackThread;
+
     type Track_Access is access TrackThread;
     package Track_Vector is new Vectors(Track_ID, Track_Access);
     package SteeringToTracks_Vector is new Vectors(Steering_ID, Track_Access);
@@ -36,7 +40,7 @@ procedure Main is
     Steerings : Steering_Vector.Vector;
     Train1Route : Steering_Vector.Vector;
 
-    task type TrainThread(ID : Train_ID) is
+    task type TrainThread(ID : Train_ID; Velocity : Integer) is
         entry Init(Init_Route : in Steering_Vector.Vector);
     end TrainThread;
     type Train_Access is access TrainThread;
@@ -55,28 +59,41 @@ procedure Main is
         My_TrackType : Track_Type;
         My_Steering: Steering_ID;
         Next_Steering: Steering_ID;
+        Track_To_Release : Track_Access;
+        Wait_Time : Integer;
+        Track_MaxVelocity : Integer;
+        Track_Length : Integer;
     begin
         accept Init(Init_Route : in Steering_Vector.Vector) do
             My_Route := Init_Route;
         end Init;
         loop
+        Put_Line("TrainThread");
+        My_Steering := My_Route(My_Route.First_Index).ID;
+        Next_Steering := My_Route(My_Route.First_Index).ID;
+        Put_Line(Steering_ID'Image(My_Steering) & " " & Steering_ID'Image(Next_Steering));
+        My_Route(My_Steering).Request_TravelThroug(ID, Next_Steering, My_Track);
             for I in Steering_ID range My_Route.First_Index .. My_Route.Last_Index - 1 loop
-                Put_Line("TrainThread");
-                My_Steering := My_Route(I).ID;
-                Next_Steering := My_Route(I+1).ID; 
-                My_Route(My_Steering).Request_TravelThroug(ID, Next_Steering, My_Track);
                 Put_Line("TrainThread: "& Train_ID'Image (ID) & " on track");
                 My_Track.Check_TrackType(My_TrackType);
                 case My_TrackType is
                     when Stop_Track =>
                         Put_Line("I am stop track");
-                        delay 5.0;
+                        My_Track.Wait_OnStation(Wait_Time);
+                        delay Duration(Wait_Time);
                     when Drive_Track =>
                         Put_Line("I am drive track");
-                        delay 15.0;
+                        My_Track.Drive_Trough(Track_Length, Track_MaxVelocity);
+                        delay Duration(Track_Length/Track_MaxVelocity);
                 end case;
-                My_Track.Release_Track(ID);
+                Track_To_Release := My_Track;
+                My_Steering := My_Route(I).ID;
+                Next_Steering := My_Route(I+1).ID; 
+                Put_Line(Steering_ID'Image(My_Steering) & " " & Steering_ID'Image(Next_Steering));
+                My_Route(My_Steering).Request_TravelThroug(ID, Next_Steering, My_Track);
+                Track_To_Release.Release_Track(ID);
             end loop;
+        My_Track.Release_Track(ID);
         end loop;
     end TrainThread;
 
@@ -89,8 +106,7 @@ procedure Main is
         loop
             accept Request_TravelThroug(TrainID : in Train_ID; Next_Steering : in Steering_ID;
                    Track_InUse : out Track_Access)  do 
-                    Put_Line("MY STEERING ID:" & Steering_ID'Image(ID));
-                    Put_Line("MY STEERING ID:" & Steering_ID'Image(Next_Steering));
+                    Put_Line("steering id: " & Steering_ID'Image(ID) & " Target steering: " & Steering_ID'Image(Next_Steering));
                     My_Neighbours(Next_Steering).Assign_Train(TrainID);
                     Track_InUse := My_Neighbours(Next_Steering); 
             end Request_TravelThroug;
@@ -122,15 +138,26 @@ procedure Main is
         begin
             TrackType := My_TrackType;
         end;
+        entry Wait_OnStation(Wait_OnTrackTime : out Integer)
+        when not Clear is
+        begin
+             Wait_OnTrackTime := Wait_Time;  
+        end;
+        entry Drive_Trough(Length : out Integer; MaxVelocity : out Integer)
+        when not Clear is
+        begin
+            Length := Track_Length;
+            MaxVelocity := Track_Max_Velocity;
+        end;
     end TrackThread;
     Trains : Train_Vector.Vector;
 
 begin
-    StopTracks.Append(new TrackThread(1, Stop_Track));
-    StopTracks.Append(new TrackThread(2, Stop_Track));
-    StopTracks.Append(new TrackThread(3, Stop_Track));
-    StopTracks.Append(new TrackThread(4, Stop_Track));
-    DriveTracks.Append(new TrackThread(1, Drive_Track));
+    StopTracks.Append(new TrackThread(1, Stop_Track, 5, 0, 0));
+    StopTracks.Append(new TrackThread(2, Stop_Track, 5, 0, 0));
+    StopTracks.Append(new TrackThread(3, Stop_Track, 5, 0, 0));
+    StopTracks.Append(new TrackThread(4, Stop_Track, 5, 0, 0));
+    DriveTracks.Append(new TrackThread(1, Drive_Track, 0, 10, 20));
 
     Steering1Neighbours.Append(StopTracks(1));
     Steering1Neighbours.Append(StopTracks(2));
@@ -167,7 +194,7 @@ begin
     Train1Route.Append(Steerings(3));
     Train1Route.Append(Steerings(2));
     Train1Route.Append(Steerings(1));
-    Trains.Append(new TrainThread(1));
+    Trains.Append(new TrainThread(ID => 1, Velocity=>1));
 
     Trains(1).Init(Train1Route);
 end Main;
