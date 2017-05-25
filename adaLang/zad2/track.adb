8b3391e70ecdae5dfeac2271e88459e6d52a5e68
gravoo <gravoo@yahoo.com>
2017-05-26 00:26:24 +0200

    added package for tracks

---
+++ adaLang/zad2/track.adb
@@ -0,0 +1,55 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+package body Track is
+    protected body Track_Thread is
+    procedure Init_Stop_Track(ID : Track_ID ;Track_Time_To_Wait : Natural) is 
+    begin
+        My_Type := Stop_Track;
+        My_Time_To_Wait := Track_Time_To_Wait;
+        My_ID := ID;
+    end;
+    procedure Init_Drive_Track(ID : Track_ID ;Track_Max_Velocity : Natural; Track_Length : Natural) is
+    begin
+        My_Type := Drive_Track;
+        My_Max_Velocity := Track_Max_Velocity;
+        My_Length := Track_Length;
+        My_ID := ID;
+    end;
+    procedure Get_Max_Velocity_And_Length(Length : out Natural; Max_Velocity : out Natural) is
+    begin
+        Length := My_Length;
+        Max_Velocity := My_Max_Velocity;
+    end;
+    function Get_Track_Type return Track_Type is
+    begin
+        return My_Type;
+    end;
+    function Get_Time_To_Wait return Natural is
+    begin
+        return My_Time_To_Wait;
+    end;
+    function Get_ID return Track_ID is
+    begin
+        return My_ID;
+    end;
+    entry Wait_For_Availalbe
+    when My_Availablity is
+        begin 
+            null;
+        end;
+    entry Request_Travel_Through
+    when My_Availablity is
+        begin
+            Put_Line("Track_Thread id:" & Track_ID'Image(My_ID) & " is taken");
+            My_Availablity := False;
+        end;
+    entry Request_Release_Track 
+    when not My_Availablity is
+        begin
+            Put_Line("Track_Thread id:" & Track_ID'Image(My_ID) & "released");
+            My_Availablity := True;
+        end;
+
+
+    end Track_Thread;
+end Track;
---
+++ adaLang/zad2/track.ads
@@ -0,0 +1,28 @@
+with Ada.Containers.Vectors;
+use Ada.Containers;
+
+package Track is
+    type Edges_ID is range 100..200;
+    subtype Track_ID is Edges_ID range 100..200;
+    type Track_Type is (Stop_Track, Drive_Track);
+        protected type Track_Thread is
+        procedure Init_Stop_Track(ID : Track_ID ; Track_Time_To_Wait : Natural);
+        procedure Init_Drive_Track(ID : Track_ID ;Track_Max_Velocity : Natural; Track_Length : Natural);
+        procedure Get_Max_Velocity_And_Length(Length : out Natural; Max_Velocity : out Natural);
+        function Get_Track_Type return Track_Type;
+        function Get_Time_To_Wait return Natural;
+        function Get_ID return Track_ID;
+        entry Wait_For_Availalbe;
+        entry Request_Travel_Through;
+        entry Request_Release_Track;
+        private
+            My_Type : Track_Type;
+            My_Time_To_Wait : Natural;
+            My_Max_Velocity : Natural;
+            My_Length : Natural;
+            My_Availablity : Boolean := True;
+        My_ID : Track_ID := 100;
+    end Track_Thread;
+    type Track_Thread_Access is access Track_Thread;
+    package Track_Container is new Vectors (Edges_ID, Track_Thread_Access);
+end Track;
