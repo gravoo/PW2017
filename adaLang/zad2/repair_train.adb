03f95b53fc3904df10487f3319db8051caa4d50f
gravoo <gravoo@yahoo.com>
2017-05-30 15:02:23 +0200

    added test suite for dfs

--- adaLang/zad2/repair_train.adb
+++ adaLang/zad2/repair_train.adb
@@ -1,10 +1,11 @@
-with Ada.Containers.Hashed_Maps; use Ada.Containers;
+with path_finder; use path_finder;
 package body Repair_Train is
     task body Repair_Train_Thread  is
         My_First_Steering : Node_ID := Node_ID'First;
+        My_Track : Stack_Container.List;
     begin
         accept Request_Repair_Broken_Node(Broken_Node : Node_ID)  do
-            null;
+            My_Track := Get_Path_To_Node(My_First_Steering, Broken_Node);
         end Request_Repair_Broken_Node;
     end;
 end Repair_Train;
